816a770e2c2ef53bac7de4a25124d1cf29b76457
Vivian Gichure <viviangichure@gmail.com>
2025-05-28 12:25:52 +0300

    math assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+//add
+function add(a, b) {
+  return a + b;
+}
+//subtract
+function subtract(a, b) {
+  return a - b;
+}
+//multiply
+function multiply(a, b) {
+  return a * b;
+}
+//divide
+function divide(a, b) {
+  return a / b;
+}
+//increment
+function increment(n) {
+  return n + 1;
+}
+//decrement
+function decrement(n) {
+  return n - 1;
+}
+//makeInt
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+//preserveDecimal
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
